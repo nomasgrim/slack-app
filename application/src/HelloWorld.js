f4ec57778873615443e2aeb1b08403b807750a34
nomasgrim <nomasgrim@Cliffs-MBP.attlocal.net>
2018-07-31 20:21:43 -0700

    added create-reate app, added file of own to understand import and export

---
+++ application/src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <h1>Cliff Helloed</h1>
+        );
+    }
+    
+}
+
+export default HelloWorld
